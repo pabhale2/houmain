3b5048d56e93eafe723a6c7dd7151dcffc200fb3
Abhijit Hargude <hargudeabhijit1@gmail.com>
2021-04-07 11:43:24 +0400

    updating username and password

--- code/server-app/src/main/java/com/youricsoft/houmain/model/PropertyUnit.java
+++ code/server-app/src/main/java/com/youricsoft/houmain/model/PropertyUnit.java
@@ -3,6 +3,7 @@ package com.youricsoft.houmain.model;
 import java.util.Date;
 import java.util.List;
 
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
