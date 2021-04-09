514bb047af91d98bc8f186b1fdddad48da5b8ba8
Abhijit Hargude <hargudeabhijit1@gmail.com>
2021-04-10 00:40:38 +0400

    Property Updated 1. Upload Photos API working 2. Return Photos API 3. Created API to get list for unSold Property

---
+++ code/server-app/src/main/java/com/youricsoft/houmain/dto/PropertyPhotosDTO.java
@@ -0,0 +1,20 @@
+package com.youricsoft.houmain.dto;
+
+import java.io.File;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PropertyPhotosDTO {
+	private long propertyPhotosId;
+	private long propertyId;
+	private long unitId;
+	private File file;
+	private String photoType;
+	private String photoCategory;
+	private boolean active;
+}
--- code/server-app/src/main/java/com/youricsoft/houmain/repository/PropertyRepository.java
+++ code/server-app/src/main/java/com/youricsoft/houmain/repository/PropertyRepository.java
@@ -1,9 +1,39 @@
 package com.youricsoft.houmain.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import com.youricsoft.houmain.dto.PropertyDTO;
 import com.youricsoft.houmain.model.Property;
 
 public interface PropertyRepository extends CrudRepository<Property, Long> {
-
+	
+	@Query(value =  
+			" select new com.youricsoft.houmain.dto.PropertyDTO(" + 
+			"    prop.propertyId, " + 
+			"    prop.propertyName, " + 
+			"    prop.lat, " + 
+			"    prop.lng, " + 
+			"    prop.status, " + 
+			"    prop.propertyDescription, " + 
+			"    prop.address, " + 
+			"    prop.city, " + 
+			"    prop.state, " + 
+			"    prop.country, " + 
+			"    prop.zipCode, " + 
+			"    prop.updatedBy, " + 
+			"    prop.updatedOn, " + 
+			"    prop.propertyCode, " +
+			"    propertyType.typeId, " +
+			"    propertyType.type, " +
+			"    propertyType.description) " +
+			" from " + 
+			"    Property prop join PropertyType propertyType on prop.propertyType.typeId = propertyType.typeId " + 
+			"    left join PropertyOccupancy occu on prop.propertyId = occu.propertyId " 
+			+ " where " + 
+			"    occu.id=NULL"
+			)
+	public List<PropertyDTO> findUnSoldProperties();
 }
